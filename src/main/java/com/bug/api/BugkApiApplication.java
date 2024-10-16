c23f6262af8212a3d7523e01915728140fc8e28b
aparnanashte <110103438+aparnanashte@users.noreply.github.com>
2024-10-16 21:17:00 +0530

    Update BugkApiApplication.java

--- src/main/java/com/bug/api/BugkApiApplication.java
+++ src/main/java/com/bug/api/BugkApiApplication.java
@@ -138,8 +138,7 @@ public class BugkApiApplication {
 	            String content = new String(Files.readAllBytes(Paths.get(filePath)));
 	            System.out.println("File content:\n" + content);
 		System.out.println("File path:\n" + filePath);
-		   String workingDir = System.getProperty("user.dir");
-    	           System.out.println("Current Directory" + workingDir);
+		
 	 
 	        } catch (GitAPIException e) {
 	            e.printStackTrace();
