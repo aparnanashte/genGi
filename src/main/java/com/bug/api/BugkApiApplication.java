f9e22d325c81fb438fbb23ae9f83a430a55e0719
aparnanashte <110103438+aparnanashte@users.noreply.github.com>
2024-10-18 01:30:01 +0530

    Update BugkApiApplication.java

--- src/main/java/com/bug/api/BugkApiApplication.java
+++ src/main/java/com/bug/api/BugkApiApplication.java
@@ -1,11 +1,8 @@
 package com.bug.api;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.jgit.api.Git;
@@ -21,48 +18,32 @@ import com.google.cloud.vertexai.api.GenerateContentResponse;
 import com.google.cloud.vertexai.generativeai.GenerativeModel;
 import com.google.cloud.vertexai.generativeai.ResponseHandler;
 
-
 @SpringBootApplication
 public class BugkApiApplication {
 	public static void main(String[] args) {
-String projectId = "glb-fs-wgh-app-dev";
+		String projectId = "glb-fs-wgh-app-dev";
 		String secretName = "github-token-fs-4-19-new";
-
 		String location = "europe-west3";
 		String modelName = "gemini-1.5-flash-001";
-		String textPrompt = "Genrate Junit Test case for com.bug.api.model.domain.Bug java class along with import statement;";
-
-		String output = null;
-		try {
-			output = textInput(projectId, location, modelName, textPrompt);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		String textPrompt = args[0];
 		String workingDir = System.getProperty("user.dir");
 		System.out.println("Current Directory" + workingDir);
-		String repoUrl = "https://github.com/aparnanashte/genGi.git"; // Replace with the public repo URL
+		String repoUrl = "https://github.com/aparnanashte/genGi.git"; 
 
 		// Access Secret Manager
 		SecretManagerServiceClient client = null;
 		try {
 			client = SecretManagerServiceClient.create();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		SecretVersionName secretVersionName = SecretVersionName.of(projectId, secretName, "latest");
-		String accessToken = client.accessSecretVersion(secretVersionName).getPayload().getData().toStringUtf8();
-		System.out.println("accessToken: " + accessToken);
-		CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider("aparnanashte@gmail.com",
-				accessToken);
+			SecretVersionName secretVersionName = SecretVersionName.of(projectId, secretName, "latest");
+			String accessToken = client.accessSecretVersion(secretVersionName).getPayload().getData().toStringUtf8();
+			System.out.println("accessToken: " + accessToken);
+			CredentialsProvider credentialsProvider = new UsernamePasswordCredentialsProvider("aparnanashte@gmail.com",
+					accessToken);
 
-		File localDir = new File(workingDir);
-		Git git = null;
-		try {
+			File localDir = new File(workingDir);
+			Git git = null;
 
 			if (localDir.exists() && localDir.isDirectory()) {
-
 				git = Git.open(localDir);
 				git.pull().call();
 				System.out.println("Pulled the latest changes.");
@@ -75,79 +56,35 @@ String projectId = "glb-fs-wgh-app-dev";
 
 			}
 
+			List<String> fieNames = FileFinder.findJavaFiles(workingDir);
+			String testfileName = null;
+			for (String name : fieNames) {
+				String name1 = name.substring(name.lastIndexOf("/"), name.lastIndexOf("."));
+
+				textPrompt = "Genrate Junit Test case for " + name1 + "class along with import statement";
+				testfileName = workingDir + "/" + name1 + "Test.java";
+
+				try (FileWriter writer = new FileWriter(testfileName)) {
+
+					writer.write(textInput(projectId, location, modelName, textPrompt));
+					System.out.println("File created successfully: " + testfileName);
+				} catch (IOException e) {
+					System.out.println("An error occurred while creating the file: " + e.getMessage());
+				}
+
+			}
+
+			git.add().addFilepattern(".").call();
+			git.commit().setMessage("Commit messag2222").call();
+			git.push().setCredentialsProvider(credentialsProvider).call();
+			git.close();
 		} catch (GitAPIException | IOException e) {
 			e.printStackTrace();
 		}
-
-		// Clone the repository
-		/*
-		 * if (Files.notExists(Paths.get(workingDir))) { git =
-		 * Git.cloneRepository().setURI(repoUrl).setDirectory(new File(workingDir))
-		 * .setCredentialsProvider(credentialsProvider).call(); }else {
-		 * git.pull().setCredentialsProvider(credentialsProvider).call(); }
-		 */
-
-		// Read the specific file from the cloned repo
-		/*File newDir = new File(workingDir+"/GenratedResult");            
-		if (newDir.mkdir()) {
-			System.out.println("Directory created: " + newDir.getAbsolutePath());
-		} else {
-			System.out.println("Failed to create directory or it already exists.");
-		}*/
-		
-
-		List<String> fieNames = FileFinder.findJavaFiles(workingDir);
-		Path folderPath = Paths.get("NewGenratedResult"); 
-		String testfileName = null;
-		for (String name : fieNames) {
-			
-			String name1= name.substring(name.lastIndexOf("/")+1,name.lastIndexOf("."));
-
-			textPrompt = "Genrate Junit Test case for " + name1 + "class along with import statement";
-			testfileName =   name1 + "Test.java";
-			Path filePath = folderPath.resolve(testfileName); 
-
-			try {
-				output = textInput(projectId, location, modelName, textPrompt);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			
-
-			try  {
-				Files.write(filePath, Collections.singleton(output)); 
-				System.out.println("File created successfully: " + filePath.toAbsolutePath());
-			} catch (IOException e) {
-				System.out.println("An error occurred while creating the file: " + e.getMessage());
-			}
-
-		}
-		try {
-			git.add().addFilepattern(folderPath.toString()).call();
-			git.commit().setMessage("Commit messag2222").call();
-			// CredentialsProvider credentialsProvider = new
-			// UsernamePasswordCredentialsProvider(username, password);
-			// Replace with your Secret Manager secret project ID and secret name
-
-			// Create credentials provider
-
-			git.push().setCredentialsProvider(credentialsProvider).call();
-			git.close();
-		} catch (GitAPIException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		/*
-		 * } catch (GitAPIException e) { e.printStackTrace(); }
-		 */
 	}
 
 	public static String textInput(String projectId, String location, String modelName, String textPrompt)
 			throws IOException {
-		// Initialize client that will be used to send requests. This client only needs
-		// to be created once, and can be reused for multiple requests.
 		try (VertexAI vertexAI = new VertexAI(projectId, location)) {
 			GenerativeModel model = new GenerativeModel(modelName, vertexAI);
 
